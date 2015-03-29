4358e10cdf27e490cc2664d25676f37ff976eecd
Ian Truslove <ian.truslove@sonian.net>
2015-03-29 20:08:31 +0000

    Simplified instrumentation a tad

--- resources/web/js/analytics.js
+++ resources/web/js/analytics.js
@@ -1,16 +1,41 @@
 $(function () {
-  var navEvent = function (navTarget) {
+  var navEvent, actionEvent, linkEvent;
+
+  navEvent = function (navTarget) {
     ga('send', 'event', 'main-nav', 'click', navTarget);
   };
-  var linkEvent = function (linkTarget) {
+
+  actionEvent = function (context, action) {
+    ga('send', 'event', context, action);
+  }
+
+  linkEvent = function (linkTarget) {
     ga('send', 'event', 'link', 'click', linkTarget);
   }
 
-  $(".instrument").click(function (ev) {
-    linkEvent(ev.target.href);
-  });
+  parentArticle = function (el) {
+    var parent = $(el).parent()[0];
+    if (parent === undefined) {
+      console.error("No article parent found.")
+      return undefined;
+    } else if (parent.tagName === "ARTICLE") {
+      return parent;
+    } else {
+      return parentArticle(parent);
+    }
+  };
 
-  $(".analytics-page-nav").click(function (ev) {
-    navEvent(ev.target.hash);
+  $("a").click(function (ev) {
+    var $target = $(ev.target);
+    if ($target.hasClass("no-instrument")) {
+      return;
+    } else if ($target.attr("data-action")) {
+      actionEvent(parentArticle(ev.target).id,
+                  $target.attr("data-action"));
+    } else if ($target.hasClass("analytics-page-nav")) {
+      navEvent(ev.target.hash);
+    } else {
+      linkEvent(ev.target.href);
+    }
   });
 });
